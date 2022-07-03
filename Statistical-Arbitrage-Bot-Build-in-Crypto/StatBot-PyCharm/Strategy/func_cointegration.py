5ebbc5d9ded7dde4acf140795162097d53379405
mindeleven <mindeleven@posteo.de>
2022-07-03 14:59:08 +0200

    Fixed typo

--- Statistical-Arbitrage-Bot-Build-in-Crypto/StatBot-PyCharm/Strategy/func_cointegration.py
+++ Statistical-Arbitrage-Bot-Build-in-Crypto/StatBot-PyCharm/Strategy/func_cointegration.py
@@ -2,7 +2,7 @@
 # Calculate cointegrated pairs
 def get_cointegrated_pairs(prices):
     # Loop through all the coins and check for co-integration
-    code_pair_list = []
+    coint_pair_list = []
     included_list = []
     for sym_1 in prices.keys():
         print(sym_1)
