a5438662faa281bba9d070c235458ac08d965f39
mindeleven <mindeleven@posteo.de>
2022-07-13 10:22:54 +0200

    Elaborating on step 3 and 4 of the strategy

--- Statistical-Arbitrage-Bot-Build-in-Crypto/StatBot-PyCharm/Strategy/main.py
+++ Statistical-Arbitrage-Bot-Build-in-Crypto/StatBot-PyCharm/Strategy/main.py
@@ -5,10 +5,13 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 from func_get_symbols import get_tradeable_symbols
 from func_prices_json import store_price_history
 from func_cointegration import get_cointegrated_pairs
+from func_plot_trends import plot_trends
 import pandas as pd
 import json
 import os.path
 from config_strategy_api import data_file
+from config_strategy_api import cointegrated_pairs_file
+
 
 """STRATEGY CODE"""
 if __name__ == '__main__':
@@ -27,9 +30,24 @@ if __name__ == '__main__':
     else :
         print("File with historic data already exists\n")
 
-    # STEP 3 - Find cointegrated pairs
-    print("Calculating co-integration...")
+    if os.path.isfile(cointegrated_pairs_file) == False:
+
+        # STEP 3 - Find cointegrated pairs
+        print("Calculating co-integration...")
+        with open(data_file) as json_file:
+            price_data = json.load(json_file)
+            if len(price_data) > 0:
+                coint_pairs = get_cointegrated_pairs(price_data)
+        print("done.")
+
+    else :
+        print("File with co-integrated pairs already exists\n")
+
+    # STEP 4 - Plot trends and safe for backtesting
+    print("Plotting trends....")
+    symbol_1 = "BNBUSDT"
+    symbol_2 = "OMGUSDT"
     with open(data_file) as json_file:
         price_data = json.load(json_file)
         if len(price_data) > 0:
-            coint_pairs = get_cointegrated_pairs(price_data)
+            plot_trends(symbol_1, symbol_2, price_data)
