9aaa57633aa20915ec2b2aa5286441cac466d8d4
mindeleven <mindeleven@posteo.de>
2022-07-23 10:41:52 +0200

    Storing constants in dictionary

---
+++ Algorythimc-Forex-Trading/code/constants/defs.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+account_data = pd.read_csv("~/Documents/temp/oanda/testnet_key.txt", sep=" ", header=None)
+
+defs = {
+    'API_KEY': account_data[0][2],
+    'ACCOUNT_ID': account_data[0][1],
+    'OANDA_URL': account_data[0][0]
+}
