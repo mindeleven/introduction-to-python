24af3a9e800f0b92c5bcd540351cf681dc0a85f2
mindeleven <mindeleven@posteo.de>
2022-07-25 11:19:13 +0200

    Fixed typo

--- Algorythimc-Forex-Trading/code/constants/defs.py
+++ Algorythimc-Forex-Trading/code/constants/defs.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 account_data = pd.read_csv("~/Documents/temp/oanda/testnet_key.txt", sep=" ", header=None)
 
-API_KEY: account_data[0][2]
-ACCOUNT_ID: account_data[0][1]
-OANDA_URL: account_data[0][0]
+API_KEY = account_data[0][2]
+ACCOUNT_ID = account_data[0][1]
+OANDA_URL = account_data[0][0]
