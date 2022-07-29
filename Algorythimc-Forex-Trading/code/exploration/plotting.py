af378ddac54a30bcd2131cd7ab0ee65f5ecba3ad
mindeleven <mindeleven@posteo.de>
2022-07-29 12:14:36 +0200

    Added add_traces funtionality to CandlePlot class

--- Algorythimc-Forex-Trading/code/exploration/plotting.py
+++ Algorythimc-Forex-Trading/code/exploration/plotting.py
@@ -60,7 +60,19 @@ class CandlePlot:
             plot_bgcolor="#2c303c",
             font = dict(size=8, color="#e1e1e1")
         )
+    
+    # line_traces are the columns that should be included
+    def add_traces(self, line_traces):
+        for t in line_traces:
+            self.fig.add_trace(go.Scatter(
+            x = self.df_plot.sTime,
+            y = self.df_plot[t],
+            line = dict(width = 2),
+            line_shape='spline',
+            name=t
+        ))
 
-    def show_plot(self, width=900, height=400, nticks=5):
+    def show_plot(self, width=900, height=400, nticks=5, line_traces=[]):
+        self.add_traces(line_traces)
         self.update_layout(width, height, nticks)
         self.fig.show()
