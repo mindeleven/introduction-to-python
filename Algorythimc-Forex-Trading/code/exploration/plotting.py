4f95241112a6f9af2b46811c7a15cee7499f73ea
mindeleven <mindeleven@posteo.de>
2022-08-02 15:30:40 +0200

    Added parameter to constructor of CandlePlot class to allow for plotting of simple lines

--- Algorythimc-Forex-Trading/code/exploration/plotting.py
+++ Algorythimc-Forex-Trading/code/exploration/plotting.py
@@ -5,9 +5,11 @@ import datetime as dt
 
 class CandlePlot:
 
-    def __init__(self, df):
+    def __init__(self, df, candles=True):
         # working with copy of original data frame
         self.df_plot = df.copy()
+        # add boolean to decide if candles or lines will be plotted
+        self.candles = candles
         # create candle figure on instantiation
         self.create_candle_fig()
     
@@ -21,22 +23,24 @@ class CandlePlot:
         # create figure object from the plot
         self.fig = go.Figure()
         # add trace to graph object
-        self.fig.add_trace(go.Candlestick(
-            # setting options
-            x       = self.df_plot.sTime,
-            open    = self.df_plot.mid_o,
-            high    = self.df_plot.mid_h,
-            low     = self.df_plot.mid_l,
-            close   = self.df_plot.mid_c,
-            # styling the lines
-            line    = dict(width=1),
-            opacity = 1,
-            # modifying appearance of candles
-            increasing_fillcolor = "#24A06B",
-            decreasing_fillcolor = "#CC2E3C",
-            increasing_line_color = "#2EC886",
-            decreasing_line_color = "#FF3A4C"
-        ))
+        # if self.candles not true only lines will be plotted
+        if self.candles == True:
+            self.fig.add_trace(go.Candlestick(
+                # setting options
+                x       = self.df_plot.sTime,
+                open    = self.df_plot.mid_o,
+                high    = self.df_plot.mid_h,
+                low     = self.df_plot.mid_l,
+                close   = self.df_plot.mid_c,
+                # styling the lines
+                line    = dict(width=1),
+                opacity = 1,
+                # modifying appearance of candles
+                increasing_fillcolor = "#24A06B",
+                decreasing_fillcolor = "#CC2E3C",
+                increasing_line_color = "#2EC886",
+                decreasing_line_color = "#FF3A4C"
+            ))
     
     def update_layout(self, width, height, nticks):
         self.fig.update_yaxes(
