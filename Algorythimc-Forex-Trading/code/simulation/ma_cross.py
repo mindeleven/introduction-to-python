db987055e38a01cb0bdf4aed1e62d1d3a2ea726f
mindeleven <mindeleven@posteo.de>
2022-08-03 14:34:34 +0200

    Writing out the functionality to load price data with moving averages

--- Algorythimc-Forex-Trading/code/simulation/ma_cross.py
+++ Algorythimc-Forex-Trading/code/simulation/ma_cross.py
@@ -8,7 +8,14 @@ get_ma_col = lambda x: f"MA_{x}"
 
 
 def load_price_data(pair, granularity, ma_list):
-    pass
+    # load the data frame for pair and granularity granularity
+    df = pd.read_pickle(f"../data/{pair}_{granularity}.pkl")
+    # calculate moving averages
+    for ma in ma_list:
+        df[get_ma_col(ma)] = df.mid_c.rolling(window=ma).mean()
+    df.dropna(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
 
 def analyse_pair(instrument, granularity, ma_long, ma_short):
     
@@ -16,7 +23,8 @@ def analyse_pair(instrument, granularity, ma_long, ma_short):
     pair = instrument.name
 
     # load price data
-
+    price_data = load_price_data(pair, granularity, ma_list)
+    print(price_data.head(3))
 
 def run_ma_sim(
     curr_list=["EUR", "USD", "AUD", "GBP"],
@@ -31,4 +39,4 @@ def run_ma_sim(
                 pair = f"{p1}_{p2}"
                 # check if pait in currency list
                 if pair in ic.instruments_dict.keys():
-                    print(pair)
+                    analyse_pair(ic.instruments_dict[pair], g, ma_long, ma_short)
